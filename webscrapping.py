eaefacb2b16504b71ad3151b1bd6b5f0232b4373
Joan Pau <joanpau.castells@hotmail.com>
2017-02-23 00:02:17 +0100

    Troba el titol del llibre

--- webscrapping.py
+++ webscrapping.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env/python
 # -*- coding: utf-8 -*-
 import urllib2
+from bs4 import BeautifulSoup
 """
 Client per a la web https://www.packtpub.com/packt/offers/free-learning/
 """
@@ -11,11 +12,22 @@ class Client(object):
         html=f.read()
         f.close()
         return html
-    
+    def search_text(self,html):
+        """Buscar text"""
+        soup=BeautifulSoup(html,'html.parser')
+        title=soup.find("div","dotd-title")
+        print title.text
+
+
+
+    def main(self):
+        web=client.get_web("https://www.packtpub.com/packt/offers/free-learning/")
+        client.search_text(web)
+
+
 
 
 
 if __name__ == "__main__":
     client=Client()
-    web=client.get_web("https://www.packtpub.com/packt/offers/free-learning/")
-    print web
+    client.main()
